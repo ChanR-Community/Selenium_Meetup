eed898fb8f2825170abefb1ebeb85c5a0802a695
DaLiteralPanda <yogamer0101@gmail.com>
2021-01-25 12:43:30 +0530

    Done with the yt_automation now i'll preapare to teach :)

--- session_1/yt_automation.py
+++ session_1/yt_automation.py
@@ -10,14 +10,14 @@ browser = webdriver.Chrome(PATH)
 # Going to youtube
 browser.get("https://www.youtube.com")
 # Waiting for the website to load
-sleep(3)
+sleep(1)
 
 # Identifying the search element
 search_elem = browser.find_element_by_id("search")
 
 ## Clicking the elemnt and then typing in it
 search_elem.click()
-search_elem.send_keys("Freedom by Kygo")
+search_elem.send_keys(input("Search a video less than an hr long :- "))
 
 # Importing special keys that let us press the Enter, Backspace keys etc.
 from selenium.webdriver.common.keys import Keys
@@ -27,10 +27,14 @@ search_elem.send_keys(Keys.ENTER)
 
 sleep(3)
 # Getting the song xpath
-song_xpath = "//div[@id='contents']/ytd-video-renderer/div/div/div/div/h3/a[@title='Kygo, Zak Abel - Freedom ft. Zak Abel']"
+song_xpath = "//div[@id='contents']/ytd-video-renderer/div/div/div/div/h3/a"
 
 # Finding the song element
-song_elem = browser.find_element_by_xpath(song_xpath)
+
+# We cant use find_element_by_xpath as that will not be indexed
+
+# So instead we use find_elements_by_id and click on the 1st video
+song_elem = browser.find_elements_by_xpath(song_xpath)[0]
 
 # Cliking on the song
 song_elem.click()
@@ -48,12 +52,20 @@ current_vid_duration_elem = browser.find_element_by_xpath(current_vid_duration_x
 # print(current_vid_time_elem.text)
 # print(current_vid_duration_elem.text)
 
-## Making while statment that says until current_vid_time_elem != current_vid_duration_elem
-while current_vid_time_elem != current_vid_duration_elem:
-    print(current_vid_time_elem.text)
+# Converting vid duration to seconds then waiting that much and then exiting the browser
+
+for i in range(2):
+    vid_time_split = current_vid_duration_elem.text.split(':')
+    minutes = int(vid_time_split[0]) * 60
+    seconds = int(vid_time_split[1])
+    sleep(1)
+
+sleep(minutes + seconds - 2)
 
 print("Playing is complete, exiting in some time")
-# After 5 seconds close the browser
-sleep(5)
+
+# After 3 seconds close the browser
+sleep(3)
+
 # Quiting the browser
 browser.quit()
